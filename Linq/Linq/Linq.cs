23264cf0561748816de32e4ca52e56288bc40fc5
uehara <uehara@kk-nks.co.jp>
2018-10-16 18:08:28 +0900

    FirstOrDefault ジェネリックなし版①

---
+++ Linq/Linq/Linq.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linq
+{
+    public class Linq
+    {
+        public int FirstOrDefault(int[] source)
+        {
+            foreach(var element in source)
+            {
+                if(element == 1)
+                {
+                    return element;
+                }
+            }
+            return default(int);
+        }
+
+        public bool FirstOrDefault(bool[] source)
+        {
+            foreach (var element in source)
+            {
+                if(element == true)
+                {
+                    return element;
+                }
+            }
+            return default(bool);
+        }
+
+        public string FirstOrDefault(string[] source)
+        {
+            foreach (var element in source)
+            {
+                if(element == "aaa")
+                {
+                    return element;
+                }
+
+            }
+            return default(string);
+        }
+
+        // 型が違う毎に作成………………………………
+    }
+}
